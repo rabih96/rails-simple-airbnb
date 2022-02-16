ba4286c21f571a5688e2c366a6c33967e63feef9
Rabih Mteirek <rabihmteyrek@hotmail.com>
2022-02-16 15:47:59 -0500

    Basic structure with routes and index, destroy.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Generating fake data'
+
+10.times do
+  flat = Flat.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.description,
+    price_per_night: rand(1..5) * 100,
+    number_of_guests: rand(1..7)
+  )
+end
